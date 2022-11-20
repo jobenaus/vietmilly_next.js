e53096df5fe1e2d76cdd416390f0500c62401287
jobenaus <j.obenaus@icloud.com>
2022-11-20 19:27:34 +0700

    installed firebase

---
+++ utils/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMRAfcvw-awHdYl-mCqGweNxrd78MDEgo',
+  authDomain: 'vietmilly-dev.firebaseapp.com',
+  projectId: 'vietmilly-dev',
+  storageBucket: 'vietmilly-dev.appspot.com',
+  messagingSenderId: '756416480562',
+  appId: '1:756416480562:web:99ed6729c4a7441442032b',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
