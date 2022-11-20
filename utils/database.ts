12c78ef097fe353b26579e87fea5e438a2ead133
jobenaus <j.obenaus@icloud.com>
2022-11-20 09:25:05 +0700

    prod database

--- utils/database.ts
+++ utils/database.ts
@@ -1,7 +1,7 @@
 import { BlogPostModel } from '../models/BlogModel';
 import { DiaryEntryModel } from '../models/DiaryModel';
-import blogPostsJSON from '../data/dev-blog.json';
-import diaryEntrysJSON from '../data/dev-diary.json';
+import blogPostsJSON from '../data/blog.json';
+import diaryEntrysJSON from '../data/diary.json';
 
 export const blogPosts: BlogPostModel[] = blogPostsJSON;
 export const diaryEntrys: DiaryEntryModel[] = diaryEntrysJSON;
