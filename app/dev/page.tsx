c7c31aad0a406f7058a6720ddf5b135c4f91a655
jobenaus <j.obenaus@icloud.com>
2022-11-22 11:31:19 +0700

    changed logo color added primary and second color

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,15 +1,13 @@
 import { JournalEntryModel } from '../../models/JournalModel';
 import { blogPosts } from '../../utils/database';
+import Image from 'next/image';
+import icon from '../../public/milly-color.svg';
 
 export default function Dev() {
-  const middleColor = (0.9 * 0xe02523 + 0.1 * 0xffffff).toString(16);
-  console.log(middleColor);
-  const middleClassnames = `bg-[#${middleColor}] flex-grow`;
   return (
     <div className="h-screen bg-white flex flex-col">
-      <div className="bg-red-600 flex-grow"></div>
-      <div className={middleClassnames}></div>
-      <div className="bg-red-600 opacity-90 flex-grow"></div>
+      <Image src={icon} alt="icon" className="bg-black"></Image>
+      <div className="bg-secondary flex-grow"> </div>
     </div>
   );
 }
