1c1a4a85093fe5c6fb75afd5ec083b73fe50a38e
jobenaus <j.obenaus@icloud.com>
2022-11-22 14:38:03 +0700

    styling for smallest size

--- app/(home)/JournalPagePreview.tsx
+++ app/(home)/JournalPagePreview.tsx
@@ -15,9 +15,9 @@ export default function JournalPagePreview({
   const previewJournalEntrys = journalEntrys.slice(0, previewSize);
   return (
     <div className="pb-12 flex flex-col items-center border-b-2 last:border-b-0 border-black">
-      <h1 className="text-5xl font-bold mt-12 mb-9">{previewTitle}</h1>
+      <h1 className="text-5xl font-bold mt-14 mb-12">{previewTitle}</h1>
 
-      <div className="flex flex-col sm:grid-cols-3 sm:grid gap-8">
+      <div className="flex flex-col sm:grid-cols-3 sm:grid gap-12">
         {previewJournalEntrys.map((journalEntry) => (
           <div key={journalEntry.upload_date}>
             <JournalPagePreviewItem journalEntry={journalEntry} />
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,22 +9,24 @@ import JournalPagePreview from './JournalPagePreview';
 export default function Home() {
   const previewSize = 3;
   return (
-    <div className="px-3 mx-auto max-w-lg sm:max-w-6xl">
-      <JournalPagePreview
-        journalEntrys={latestJournalEntrys}
-        previewSize={previewSize}
-        previewTitle="Neuste Journal Einträge"
-      />
-      <JournalPagePreview
-        journalEntrys={blogPosts}
-        previewSize={previewSize}
-        previewTitle="Beliebteste Blog Posts"
-      />
-      <JournalPagePreview
-        journalEntrys={diaryEntrys}
-        previewSize={previewSize}
-        previewTitle="Beliebteste Tagebuch Einträge"
-      />
+    <div className="mx-5">
+      <div className="mx-auto max-w-lg sm:max-w-6xl">
+        <JournalPagePreview
+          journalEntrys={latestJournalEntrys}
+          previewSize={previewSize}
+          previewTitle="Neuste Journal Einträge"
+        />
+        <JournalPagePreview
+          journalEntrys={blogPosts}
+          previewSize={previewSize}
+          previewTitle="Beliebteste Blog Posts"
+        />
+        <JournalPagePreview
+          journalEntrys={diaryEntrys}
+          previewSize={previewSize}
+          previewTitle="Beliebteste Tagebuch Einträge"
+        />
+      </div>
     </div>
   );
 }
