38164679555e920f1be95d061ef0d92d6a709113
jobenaus <j.obenaus@icloud.com>
2022-11-23 14:43:51 +0700

    favicon.svg

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,7 @@ import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '../public/milly-color.svg';
+import logo from '../public/favicon.svg';
 
 const iconHref = '/';
 
