2c5285d0c6d69d29432d235516828b3a5792e49a
jobenaus <j.obenaus@icloud.com>
2022-11-27 02:36:10 +0700

    adapts to skew

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -9,11 +9,23 @@ export default function Dev() {
     width: '80vw',
   };
 
+  console.log(Math.tan((45 * Math.PI) / 180));
+
   const [leftWidth, setLeftWidth] = useState('60%');
+
+  // tan (90-12) =  h / w -> w = h / tan (90° - 12°) = h / tan ((90-12)*Math.PI/180)
+  // h = (innerHeight/2) + y
 
   useEffect(() => {
     const handleMove = (e: Touch | MouseEvent) => {
-      setLeftWidth(`calc(${(e.clientX / window.innerWidth) * 100}% + 80px)`);
+      setLeftWidth(
+        `calc(${(e.clientX / window.innerWidth) * 100}% 
+         - ${
+           (window.innerHeight / 2 - e.clientY) /
+           Math.tan(((90 - 12) * Math.PI) / 180)
+         }px)`
+      );
+      console.log(e.clientX, e.clientY, window.innerWidth, window.innerHeight);
     };
 
     document.onmousemove = (e) => handleMove(e);
@@ -25,10 +37,10 @@ export default function Dev() {
     <>
       <div
         id="left-side"
-        className="bg-blue-200 z-20 h-screen overflow-hidden grid place-items-center absolute -skew-x-12 -translate-x-20"
+        className="bg-blue-200 z-20 h-screen overflow-hidden grid place-items-center absolute -skew-x-12"
         style={{ width: leftWidth }}
       >
-        <h2 className="text-white skew-x-12 translate-x-20" style={titleStyle}>
+        <h2 className="text-white skew-x-12" style={titleStyle}>
           Vietmilly Journal
         </h2>
       </div>
