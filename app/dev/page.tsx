f3034a2ffdbc447ab43543e19fd50826a34c77a4
jobenaus <j.obenaus@icloud.com>
2022-11-26 10:38:13 +0700

    copied tutorial

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,15 +1,45 @@
-import SwiperCarousel from '../(journal)/(journal-entry)/SwiperCarousel';
-import { JournalImgModel } from '../../models/JournalModel';
+'use client';
 
-import { blogPosts } from '../../utils/database';
-import NewCarousel from './NewCarousel';
+import { useEffect } from 'react';
 
 export default function Dev() {
-  const imgs: JournalImgModel[] = blogPosts[0].sections[10].startImgs!;
+  // const left = document.getElementById('left-side');
+
+  const titleStyle = {
+    fontSize: '8vw',
+    margin: '20vw',
+    width: '80vw',
+  };
+
+  useEffect(() => {
+    // id name is "my-btn"
+    const left = document.getElementById('left-side');
+    console.log(left);
+    const handleMove = (e) => {
+      left.style.width = `${(e.clientX / window.innerWidth) * 100}%`;
+    };
+
+    document.onmousemove = (e) => handleMove(e);
+
+    document.ontouchmove = (e) => handleMove(e.touches[0]);
+  }, []);
 
   return (
-    <div className="w-[500px] aspect-square border mx-auto mt-20">
-      <NewCarousel journalImgs={imgs} />
-    </div>
+    <>
+      <div
+        id="left-side"
+        className="bg-blue-200 w-3/5 z-20 h-screen overflow-hidden grid place-items-center absolute"
+      >
+        <h2 className="text-white" style={titleStyle}>
+          Vietmilly Journal
+        </h2>
+      </div>
+
+      <div className="bg-orange-200 h-screen overflow-hidden w-full grid place-items-center absolute">
+        <h2 className="text-black" style={titleStyle}>
+          Vietmilly Journal
+        </h2>
+      </div>
+    </>
   );
 }
