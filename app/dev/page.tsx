565aa53a66ebb434374923816653329091d0a6bd
jobenaus <j.obenaus@icloud.com>
2022-11-28 12:11:22 +0700

    working on scroll

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { ScrollbarEvents } from 'swiper/types';
 
 export default function Dev() {
   const titleStyle = {
@@ -21,26 +22,44 @@ export default function Dev() {
       setLeftWidth(
         `calc(${(e.clientX / window.innerWidth) * 100}% 
          - ${
-           (window.innerHeight / 2 - e.clientY) /
+           (window.innerHeight / 2 - e.clientY - window.scrollY + 70) /
            Math.tan(((90 - 12) * Math.PI) / 180)
-         }px)`
+         }px + 80px)`
       );
-      console.log(e.clientX, e.clientY, window.innerWidth, window.innerHeight);
+      console.log(
+        e.clientX,
+        e.clientY,
+        window.innerWidth,
+        window.innerHeight,
+        window.scrollY
+      );
+    };
+
+    const handleScroll = (e: ScrollbarEvents) => {
+      setLeftWidth((prev) => {
+        return `calc(${prev}% 
+         - ${
+           (window.innerHeight / 2 - window.scrollY) /
+           Math.tan(((90 - 12) * Math.PI) / 180)
+         }px + 80px)`;
+      });
     };
 
     document.onmousemove = (e) => handleMove(e);
 
     document.ontouchmove = (e) => handleMove(e.touches[0]);
+
+    document.onscroll = (e) => handleScroll(e);
   }, []);
 
   return (
     <>
       <div
         id="left-side"
-        className="bg-blue-200 z-20 h-screen overflow-hidden grid place-items-center absolute -skew-x-12"
+        className="bg-blue-200 z-20 h-screen overflow-hidden grid place-items-center absolute -skew-x-12 -translate-x-20"
         style={{ width: leftWidth }}
       >
-        <h2 className="text-white skew-x-12" style={titleStyle}>
+        <h2 className="text-white skew-x-12 translate-x-20" style={titleStyle}>
           Vietmilly Journal
         </h2>
       </div>
@@ -50,6 +69,7 @@ export default function Dev() {
           Vietmilly Journal
         </h2>
       </div>
+      <div className="h-[200vh]"></div>
     </>
   );
 }
