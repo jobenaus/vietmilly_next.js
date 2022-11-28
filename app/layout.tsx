488799d81051dfa42c522a0ffa488fa02e689590
jobenaus <j.obenaus@icloud.com>
2022-11-29 00:24:38 +0700

    trying to install nerko

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css';
 
 import NavBar from './NavBar';
+import { Nerko_One } from '@next/font/google';
+
+const nerko_one = Nerko_One({ weight: ['400'] });
 
 export default function RootLayout({
   children,
@@ -8,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={nerko_one.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
