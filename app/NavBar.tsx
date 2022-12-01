4fdab7a4cc468a48cc1009b71dda60f344ad9a0c
jobenaus <j.obenaus@icloud.com>
2022-12-01 16:51:56 +0700

    variable height

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -27,47 +27,51 @@ export default function NavBar() {
   return (
     <Disclosure
       as="nav"
-      className="px-2 bg-blue-300 sm:bg-opacity-0 fixed inset-0 z-50 border h-16 w-full flex items-center justify-between"
+      className="px-2 bg-blue-300 sm:bg-opacity-0 fixed inset-0 z-50 h-min"
     >
       {({ open }) => (
         <>
-          <Link href={logoHref} className="shrink-0">
-            <Image src={LogoWithText} alt="LogoWithText" height={40} />
-          </Link>
-          <div className="basis-[28%]"></div>
-          <div className="hidden sm:flex justify-between basis-[627px] shrink-[0.4]">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className={classNames(FuzzyBubbles.className, 'text-base')}
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div>
-          <div className="basis-[28%]"></div>
+          <div className="flex items-center justify-between h-16 w-full">
+            <Link href={logoHref} className="shrink-0">
+              <Image src={LogoWithText} alt="LogoWithText" height={40} />
+            </Link>
+            <div className="basis-[28%]"></div>
+            <div className="hidden sm:flex justify-between basis-[627px] shrink-[0.4]">
+              {navigation.map((item) => (
+                <Link
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(FuzzyBubbles.className, 'text-base')}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+            <div className="basis-[28%]"></div>
 
-          <div className="flex gap-1 shrink-0">
-            <Image
-              src={MagnifyingGlassIcon}
-              alt="Search"
-              height={44}
-              className="ui-open:hidden"
-            />
-            <Disclosure.Button className="sm:hidden">
-              {open ? (
-                <Image
-                  src={XMarkIcon}
-                  alt="Close main menu"
-                  height={60}
-                  className="translate-x-3 translate-y-1"
-                />
-              ) : (
-                <Image src={Bars3Icon} alt="Open main menu" height={44} />
-              )}
-            </Disclosure.Button>
+            <div className="flex gap-1 shrink-0">
+              <Image
+                src={MagnifyingGlassIcon}
+                alt="Search"
+                height={44}
+                className="ui-open:hidden sm:block"
+              />
+              <Disclosure.Button className="sm:hidden">
+                {open ? (
+                  <Image
+                    src={XMarkIcon}
+                    alt="Close main menu"
+                    height={60}
+                    className="translate-x-3 translate-y-1"
+                  />
+                ) : (
+                  <Image src={Bars3Icon} alt="Open main menu" height={44} />
+                )}
+              </Disclosure.Button>
+            </div>
           </div>
+
+          <Disclosure.Panel className="sm:hidden">Panel</Disclosure.Panel>
         </>
       )}
     </Disclosure>
