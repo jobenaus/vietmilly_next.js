ac63fa4732f7fe54338c1cb136cd2c6ba6cd99b9
jobenaus <j.obenaus@icloud.com>
2022-12-04 13:56:45 +0700

    installing nex-sanity

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,13 +1,8 @@
-import { JournalEntryModel } from '../../models/JournalModel';
-import { blogPosts } from '../../utils/database';
-import Image from 'next/image';
-import icon from '../../public/milly-color.svg';
-
-export default function Dev() {
+import { createClient } from 'next-sanity';
+export default async function Dev() {
   return (
     <div className="h-screen bg-white flex flex-col">
-      <Image src={icon} alt="icon" className="bg-black"></Image>
-      <div className="bg-secondary flex-grow"> </div>
+      <div className="bg-secondary flex-grow"></div>
     </div>
   );
 }
